7b19f9ff88b4f39593b52de47b20d1d8aceeb748
Chetna0590 <71846600+Chetna0590@users.noreply.github.com>
2020-09-25 19:16:33 +0530

    Delete reducer.js

--- GoLorryTest/src/reducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-import { LoginReducer }  from './Login/reducer'
-import { DashboardReducer } from './Dashboard/Redux/reducer'
-
-export default combineReducers({
-    LoginReducer,
-    DashboardReducer
-})
