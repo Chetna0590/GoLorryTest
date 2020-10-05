e0e6b24221d9ce7fe4f675f6fa300f7bf6e7882f
Chetna Mohanty <chetna.mohanty26@gmail.com>
2020-10-05 17:54:26 +0530

    checking in changes

--- GoLorryTest/src/axios.js
+++ GoLorryTest/src/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseURL = 'http://localhost:5000' // Point to the ip where mock-server is running
+const baseURL = 'http://192.168.0.133:5000' // Point to the ip where mock-server is running
 const client = axios.create({
     baseURL
 })
